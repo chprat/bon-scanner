0c2be0af690df598287d4445f58c2f935a7a4863
chprat <49040338+chprat@users.noreply.github.com>
2025-01-28 16:54:16 +0100

    Add database module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod database;
 pub mod settings;
 
 fn main() {
@@ -7,5 +8,13 @@ fn main() {
             "Settings file {} does not exist, using defaults",
             &settings.settings_file
         );
+    }
+    if !settings.database_exists() {
+        println!(
+            "Database {} does not exist, creating it",
+            &settings.database_file
+        );
+        let database = database::Database::new(&settings.database_file);
+        database.create_database();
     }
 }
