21b896fa3f13da936ec47ca26e7c8bf630580229
chprat <49040338+chprat@users.noreply.github.com>
2025-06-06 18:08:24 +0200

    app: Initialize settings and database
    If we want to show messages when loading the config/database, we need
    the app running and therefore can't do it in the main.rs, before the app
    is initialized.

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,6 @@ pub mod ui;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    let settings = settings::Settings::new();
-    if !settings.settings_exists() {
-        println!(
-            "Settings file {} does not exist, using defaults",
-            &settings.settings_file
-        );
-    }
-    if !settings.database_exists() {
-        println!(
-            "Database {} does not exist, creating it",
-            &settings.database_file
-        );
-        let database = database::Database::new(&settings.database_file);
-        database.create_database();
-    }
     color_eyre::install()?;
     let terminal = ratatui::init();
     let result = app::App::new().run(terminal).await;
