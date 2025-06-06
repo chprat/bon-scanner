433e455ebf775ce43f4b567953e635adba8044d6
chprat <49040338+chprat@users.noreply.github.com>
2025-06-06 18:08:47 +0200

    settings: Add path for importing new bons

--- src/settings.rs
+++ src/settings.rs
@@ -2,6 +2,7 @@ use config::Config;
 use std::path::Path;
 
 pub struct Settings {
+    pub import_path: String,
     pub settings_file: String,
     pub database_file: String,
 }
@@ -9,15 +10,45 @@ pub struct Settings {
 impl Default for Settings {
     fn default() -> Self {
         let mut settings = Self {
+            import_path: Self::build_default_import_path(),
             settings_file: Self::build_default_settings_path(),
             database_file: "".to_string(),
         };
+        settings.import_path = settings.import_path();
         settings.database_file = settings.database_path();
         settings
     }
 }
 
 impl Settings {
+    pub fn import_path(&self) -> String {
+        let mut ret = Self::build_default_import_path();
+        if self.settings_exists() {
+            let settings = Config::builder()
+                .add_source(config::File::with_name(&self.settings_file))
+                .build()
+                .expect("Couldn't build settings file");
+            if let Ok(import_path) = settings.get_string("import_path") {
+                ret = import_path;
+            }
+        }
+        ret
+    }
+
+    pub fn import_path_exists(&self) -> bool {
+        let import_path = Path::new(&self.import_path);
+        import_path.exists()
+    }
+
+    fn build_default_import_path() -> String {
+        let import_dir = dirs::picture_dir().expect("Couldn't detect picture folder");
+        let import_path = Path::new(&import_dir);
+        import_path
+            .to_str()
+            .expect("Couldn't convert path to string")
+            .to_string()
+    }
+
     fn build_default_database_path() -> String {
         let home = dirs::home_dir().expect("Couldn't detect home folder");
         let home_dir = Path::new(&home);
@@ -73,6 +104,25 @@ mod tests {
     use std::env;
 
     #[test]
+    fn nonexistent_import_path() {
+        let mut settings = Settings::new();
+        settings.import_path = "nopath".to_string();
+        assert!(!settings.import_path_exists())
+    }
+
+    #[test]
+    fn existent_import_path() {
+        let mut settings = Settings::new();
+        let cur_dir = env::current_dir().expect("Couldn't get current directory");
+        settings.import_path = cur_dir
+            .join("config/")
+            .to_str()
+            .expect("Couldn't build settings file")
+            .to_string();
+        assert!(settings.import_path_exists())
+    }
+
+    #[test]
     fn nonexistent_config() {
         let mut settings = Settings::new();
         settings.settings_file = "noconfig.toml".to_string();
@@ -92,6 +142,21 @@ mod tests {
     }
 
     #[test]
+    fn read_config() {
+        let mut settings = Settings::new();
+        let cur_dir = env::current_dir().expect("Couldn't get current directory");
+        settings.settings_file = cur_dir
+            .join("config/bon-scanner.toml")
+            .to_str()
+            .expect("Couldn't build settings file")
+            .to_string();
+        settings.import_path = settings.import_path();
+        settings.database_file = settings.database_path();
+        assert_eq!(settings.import_path, "config");
+        assert_eq!(settings.database_file, "config/bon-scanner.sqlite");
+    }
+
+    #[test]
     fn nonexistent_database() {
         let mut settings = Settings::new();
         settings.database_file = "nodatabase.sqlite".to_string();
@@ -108,18 +173,5 @@ mod tests {
             .expect("Couldn't build settings file")
             .to_string();
         assert!(settings.database_exists())
-    }
-
-    #[test]
-    fn read_config() {
-        let mut settings = Settings::new();
-        let cur_dir = env::current_dir().expect("Couldn't get current directory");
-        settings.settings_file = cur_dir
-            .join("config/bon-scanner.toml")
-            .to_str()
-            .expect("Couldn't build settings file")
-            .to_string();
-        settings.database_file = settings.database_path();
-        assert_eq!(settings.database_file, "config/bon-scanner.sqlite");
     }
 }
